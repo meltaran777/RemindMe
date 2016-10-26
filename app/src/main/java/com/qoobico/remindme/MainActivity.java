dd98226421c06ad7ae5e569342b2a938074684e1
Bodia <meltaran777@mail.ru>
2016-10-26 18:44:48 +0300

    Add toolbar

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -2,14 +2,32 @@ package com.qoobico.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Bodia on 09.06.2016.
  */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        initToolbar();
     }
+    private void initToolbar() {
+        toolbar=(Toolbar) findViewById(R.id.Toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+
 }
