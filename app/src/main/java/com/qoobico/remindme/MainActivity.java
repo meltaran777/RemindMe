7b70260c4f46b41fbf6538688ab2da929ec53821
Bodia <meltaran777@mail.ru>
2016-10-28 21:48:51 +0300

    Add navigation view menu

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -2,24 +2,57 @@ package com.qoobico.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.qoobico.remindme.adapter.TabsPagerFragmentAdapter;
 
 /**
  * Created by Bodia on 09.06.2016.
  */
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TAB_ONE=0;
+    private static final int TAB_TWO=1;
+    private static final int TAB_THREE=2;
+
+
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
         initToolbar();
+        initNavigationView();
+        initTabs();
+
     }
+
+    private void initTabs() {
+        viewPager =(ViewPager) findViewById(R.id.ViewPager);
+        tabLayout =(TabLayout) findViewById(R.id.TabLayout);
+
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
     private void initToolbar() {
         toolbar=(Toolbar) findViewById(R.id.Toolbar);
         toolbar.setTitle(R.string.app_name);
@@ -30,9 +63,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
-
     }
+
     private void initNavigationView(){
         drawerLayout=(DrawerLayout) findViewById(R.id.DrawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.view_navigation_open,R.string.view_navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+
+        navigationView.setCheckedItem(R.id.menu_item_notification);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Toast.makeText(getApplicationContext(),"ITEM SELECTED",Toast.LENGTH_LONG).show();
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.menu_item_notification:
+                        showNotificationTab();
+                        Toast.makeText(getApplicationContext(),"!!!!!!!!!!!",Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(TAB_TWO);
     }
 }
---
+++ app/src/main/java/com/qoobico/remindme/fragment/ExampleFragment.java
@@ -0,0 +1,37 @@
+package com.qoobico.remindme.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qoobico.remindme.R;
+
+/**
+ * Created by Bodia on 28.10.2016.
+ */
+public class ExampleFragment extends Fragment{
+    private static final int LAYOUT=R.layout.fragment_example_layout;
+    private View view;
+
+    public static ExampleFragment getInstance(){
+        Bundle args=new Bundle();
+        ExampleFragment fragment=new ExampleFragment();
+        fragment.setArguments(args);
+
+        return  fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+
+    }
+}
